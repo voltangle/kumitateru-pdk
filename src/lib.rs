c55bb9a8d42576da347a3845c585203fc9fe8161
ggoraa <ggoraa1029@gmail.com>
2021-08-20 13:06:04 +0300

    replaced all fn's with Fn

--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,12 @@ pub struct Kumitateru {
     /// Subscribes a function to
     /// an event in any place
     /// Kumitateru allows to extend.
-    pub subscribe: fn(&str, dyn Fn()),
+    pub subscribe: Fn(&str, Fn()),
 
     /// Creates a custom command, which can
     /// be called like any other command in
     /// Kumitateru.
-    pub create_command: fn(command::Command),
+    pub create_command: Fn(command::Command),
 }
 
 // static AVAILABLE_COMMANDS: [&str; 9] = [
