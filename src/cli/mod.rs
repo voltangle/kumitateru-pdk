6eb229e4ac80fd4194b04116dbb005ad48cf3013
ggoraa <ggoraa1029@gmail.com>
2021-11-02 22:08:05 +0200

    moved Command struct to mod.rs

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,1 +1,11 @@
-pub mod command;
+#[derive(Debug)]
+pub struct Command {
+    /// A name of the function. Will be used when called from command line.
+    name: String,
+    /// Help text displayed in help menu.
+    help: String,
+    /// Args, which this command will accept.
+    args: Vec<String>,
+    /// A handler for this command.
+    func: fn(Vec<String>),
+}
