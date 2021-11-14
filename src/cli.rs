e9bddd0c3075296f72a01b9782177abbb82714cc
ggoraa <ggoraa1029@gmail.com>
2021-11-14 21:35:49 +0200

    Added structs for extending the CLI interface of kumitateru

---
+++ src/cli.rs
@@ -0,0 +1,57 @@
+type CommandArgs = Vec<CommandArgDef>;
+type Commands = Vec<Command>;
+
+#[derive(Debug)]
+pub struct Command {
+    pub name: String,
+    pub args: CommandArgs,
+    pub func: fn(Vec<CommandArgDef>),
+}
+
+impl Command {
+    fn new(name: &str, args: CommandArgs, func: fn(Vec<CommandArgDef>)) -> Self {
+        Self {
+            name: name.to_string(),
+            args,
+            func,
+        }
+    }
+}
+
+/**
+ * This struct will be passed to the function specified
+ * in `func` member variable in [Command](Command) struct.
+ * Struct name is "decoded" as Command arg definition.
+ */
+pub struct CommandArgDef {
+    pub name: String,
+    pub value: String,
+}
+
+/**
+ * This struct is used to declare command arguments. See
+ * [Command](Command) struct for more info
+ */
+#[derive(Debug)]
+pub struct CommandArgDecl {
+    pub name: String,
+    pub long_name: Option<String>,
+    pub value_name: String,
+    pub takes_value: Option<bool>,
+}
+
+impl CommandArgDecl {
+    fn new(
+        name: &str,
+        long_name: Option<String>,
+        value_name: &str,
+        takes_value: Option<bool>,
+    ) -> Self {
+        Self {
+            name: name.to_string(),
+            long_name,
+            value_name: value_name.to_string(),
+            takes_value,
+        }
+    }
+}
